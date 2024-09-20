aaace64c9d97b99fcdf4718c3df4ce3e9dec81d7
FanYin233 <yinfa@sheridancollege.ca>
2024-09-20 16:35:07 -0400

    Check Point!

--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -51,23 +51,23 @@ public class Player : MonoBehaviour
         velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.A))
         {
-            velocity += Vector3.left * acceleration * Time.deltaTime;
+            velocity += Vector3.left;
             Debug.Log("aa");
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            velocity += Vector3.right * acceleration * Time.deltaTime;
+            velocity += Vector3.right;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            velocity += Vector3.up * acceleration * Time.deltaTime;
+            velocity += Vector3.up;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            velocity += Vector3.down * acceleration * Time.deltaTime;
+            velocity += Vector3.down;
         }
 
         if (speed <= 3f)
